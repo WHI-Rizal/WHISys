fd73fc7b65a276ce86ffae692acd756e2b13c5fb
WHI-Rizal <rizal@wisatahalalindonesia.com>
2026-08-06 16:35:07 +0700

    Update BookingsModule.jsx

--- WHISys/src/app/dashboard/BookingsModule.jsx
+++ WHISys/src/app/dashboard/BookingsModule.jsx
@@ -158,7 +158,7 @@ Terima kasih telah mendaftar program perjalanan ibadah bersama kami. Berikut rin
 Mohon melengkapi 8 berkas dokumen (Paspor, KTP/Foto, Buku Nikah, Surat Sponsor, Rekening Koran, Vaksin Meningitis, Visa (Jika sudah ada), & Tiket (Jika sudah ada)).
 
 Apabila ada pertanyaan lebih lanjut, silakan hubungi tim kami.
-Terima kasih;
+Terima kasih.';
 
     const encodedMessage = encodeURIComponent(message);
     window.open(`https://api.whatsapp.com/send?phone=${cleanPhone}&text=${encodedMessage}`, '_blank');
